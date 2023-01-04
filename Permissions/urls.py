c1ed4ff5d3e9e9a6016a95312f9b56dfcf75889f
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-04 23:57:41 +0600

    Add Toolbar

--- Permissions/urls.py
+++ Permissions/urls.py
@@ -5,8 +5,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
